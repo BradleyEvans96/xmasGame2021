ff4b3d9b880457dfbac81d9d12ffcf39ba9a9251
Bradley Evans <bradley.evans@bjss.com>
2021-11-18 23:10:58 +0000

    Player movement created

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,15 +4,92 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed;
+    public float jumpForce;
+    public Transform ceilingCheck;
+    public Transform groundCheck;
+    public LayerMask groundObjects;
+    public float checkRadius;
+
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+    private float moveDirection;
+    private bool isJumping = false;
+    private bool isGrounded;
+
+    private int jumpCount;
+
+    private int maxjumpCount = 1;
+
+    private void Start()
     {
-        
+        jumpCount = maxjumpCount;
+    }
+
+    // Awake is called after all objects are initialised. Called in a random order.
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>(); // will look for a component on this GameObject (what the script is attached to) of type Rigidbody 2D
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Get Inputs
+        ProcessInputs();
+
+        //Animate
+        Animate();
+    }
+
+    // better for handling Physics, can be called multiple times per frame
+    private void FixedUpdate()
+    {
+        // Check if grounded
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundObjects);
+        if (isGrounded)
+        {
+            jumpCount = maxjumpCount;
+        }
+        // Move
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+        if (isJumping)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            jumpCount = jumpCount - 1;
+        }
+        isJumping = false;
+    }
+
+    private void Animate()
+    {
+        if (moveDirection > 0 && !facingRight)
+        {
+            FlipCharacter();
+        }
+        else if (moveDirection < 0 && facingRight)
+        {
+            FlipCharacter();
+        }
+    }
+
+    private void ProcessInputs()
+    {
+        moveDirection = Input.GetAxis("Horizontal"); // Scale of -1 -> 1
+        if (Input.GetButtonDown("Jump") && jumpCount > 0)
+        {
+            isJumping = true;
+        }
+    }
+
+    private void FlipCharacter()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
     }
 }
